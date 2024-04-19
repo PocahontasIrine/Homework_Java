3070deb321dac5e482b1db618e50fbd3f9a19d15
PocahontasIrine <43290733+PocahontasIrine@users.noreply.github.com>
2024-04-19 14:49:29 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,65 +1,10 @@
-import java.util.GregorianCalendar;
-import java.util.Scanner;
-import java.util.Calendar;
+package Irina.lab3.var3;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        Hello h1 = new Hello();
-        h1.hello();
-        task2();
-        task3();
-        task4();
-        task5();
-        task6();
-    }
+        Fraction f1 = new Fraction(3, 5);
+        Fraction f2 = new Fraction(3, 5);
 
-    public static void task2(){
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        int c = scan.nextInt();
-        int d = scan.nextInt();
-        int e = scan.nextInt();
-        System.out.println(e + " " + d + " " + c + " " + b + " " + a);
-    }
-
-    public static void task3(){
-        Scanner scan = new Scanner(System.in);
-        String a = scan.next();
-        String b = scan.next();
-        String c = scan.next();
-        System.out.println(a + "\n" + b + "\n" + c);
-        System.out.println(a + " " + b + " " + c);
-    }
-
-    public static void task4(){
-        Scanner scan = new Scanner(System.in);
-        String password = "abcd1234";
-        System.out.println("Password is: " + password);
-        String check = scan.nextLine();
-        if(password.equals(check)){ // < > <= >= == !=
-            System.out.println("Equals");
-        }else{
-            System.out.println("Not equals");
-        }
-    }
-
-    public static void task5(){
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        int c = scan.nextInt();
-        System.out.println(a * b * c);
-    }
-
-    public static void task6(){
-        Calendar claim = new GregorianCalendar(2024, 1, 10);
-        claim.set(Calendar.HOUR, 14);
-        System.out.println(claim.getTime() + " Барская Ирина");
-        Calendar delivery = new GregorianCalendar(2024, 2, 1);
-        delivery.set(Calendar.HOUR, 11);
-        delivery.set(Calendar.MINUTE, 25);
-        System.out.println(delivery.getTime() + " Барская Ирина");
+        House h1 = new House();
     }
 }
